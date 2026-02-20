6bf2d8eb69ae17a95f8895ff18927284bcbc2f08
LeFredy7476 <forgetfrederick@gmail.com>
2026-02-20 12:54:00 -0500

    better HeadWord

--- HeadWord/script.js
+++ HeadWord/script.js
@@ -1,12 +1,11 @@
-
 const wordList = [];
 const tempWordList = [];
-const usedWord = [];
+const usedWords = [];
 const fileloaderInput = document.querySelector("#fileloader");
 const timerInput = document.querySelector("#timer");
 const startbtn = document.querySelector("#start");
-const stopbtn = document.querySelector("#stop");
 const wordDisplay = document.querySelector("#word");
+const wordCountDisplay = document.querySelector("#wordcount");
 const clockDisplay = document.querySelector("#clock");
 const winbtn = document.querySelector("#win");
 const losebtn = document.querySelector("#lose");
@@ -15,10 +14,12 @@ var inGame = false;
 var currentWord = "";
 var remainingTime = 0;
 var interval = null;
-var wordWin = 0;
-var wordLost = 0;
+var winCount = 0;
+var lostCount = 0;
 
-
+const winAudio = new Audio("maximize_006.ogg");
+const loseAudio = new Audio("minimize_006.ogg");
+const endAudio = new Audio("scroll_003.ogg");
 
 function getRandomWord() {
     if (tempWordList.length == 0) {
@@ -31,7 +32,7 @@ function getRandomWord() {
     wordDisplay.innerHTML = word;
 }
 
-fileloaderInput.addEventListener("change", function(){
+function readWords() {
     const file = fileloaderInput.files[0];
     const reader = new FileReader();
 
@@ -45,16 +46,20 @@ fileloaderInput.addEventListener("change", function(){
                 wordList.push(w);
             }
         }
-        console.log(wordList);
+        // console.log(wordList);
+        wordCountDisplay.innerHTML = `${wordList.length} words`
     });
 
     if (file) {
         reader.readAsText(file);
     }
-})
+}
+
+fileloaderInput.addEventListener("change", readWords);
 
 function tickdown() {
     if (remainingTime == 0) {
+        endAudio.play();
         endResult();
     } else {
         remainingTime--;
@@ -63,36 +68,74 @@ function tickdown() {
 }
 
 startbtn.addEventListener("click", function(){
-    tempWordList.length = 0;
-    for (let i = 0; i < wordList.length; i++) {
-        tempWordList.push(wordList[i]);
+    if (!inGame) {
+        if (wordList.length == 0) {
+            return readWords();
+        }
+        tempWordList.length = 0;
+        usedWords.length = 0;
+        for (let i = 0; i < wordList.length; i++) {
+            tempWordList.push(wordList[i]);
+        }
+        inGame = true;
+        winCount = 0;
+        lostCount = 0;
+        remainingTime = parseInt(timerInput.value);
+        interval = window.setInterval(tickdown, 1000);
+        clockDisplay.innerHTML = `${remainingTime}`;
+        getRandomWord();
+    } else {
+        endResult();
     }
-    inGame = true;
-    remainingTime = parseInt(timerInput.value);
-    interval = window.setInterval(tickdown, 1000);
-    clockDisplay.innerHTML = `${remainingTime}`;
-    getRandomWord();
-})
-stopbtn.addEventListener("click", function(){
-    endResult();
-})
+});
 
-winbtn.addEventListener("click", function(){
+winbtn.addEventListener("mousedown", function(){
     if (inGame) {
-        wordWin += 1;
+        winAudio.play();
+        usedWords.push({
+            win: true,
+            word: currentWord
+        });
+        winCount += 1;
         getRandomWord();
     }
-})
-losebtn.addEventListener("click", function(){
+});
+
+losebtn.addEventListener("mousedown", function(){
     if (inGame) {
-        wordLost += 1;
+        loseAudio.play();
+        usedWords.push({
+            win: false,
+            word: currentWord
+        });
+        lostCount += 1;
         getRandomWord();
     }
-})
+});
 
 function endResult() {
     window.clearInterval(interval);
     inGame = false;
     clockDisplay.innerHTML = "Finished";
-    wordDisplay.innerHTML = `Win: ${wordWin}, Lost: ${wordLost}`
+    const score = `Win: ${winCount}, Lost: ${lostCount}`;
+    var list = '<div id="list">';
+    for (let i = 0; i < usedWords.length; i++) {
+        const entry = usedWords[i];
+        list += `<span win="${entry.win}">${entry.word}</span>`;
+    }
+    list += `<span>${currentWord}</span>`
+    list += "</div>";
+    wordDisplay.innerHTML = score + list;
+}
+
+
+var fullscreen = Document.fullscreenElement ? true : false;
+function toggleFullscreen() {
+    if (fullscreen) {
+        document.exitFullscreen();
+        fullscreen = false;
+        return;
+    }
+    document.body.parentElement.requestFullscreen();
+    fullscreen = true;
 }
